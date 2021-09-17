31b1dcdcfe01d80b8fedd1d4060532f93dd37c22
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2021-09-17 11:36:00 -0500

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num {100};
+    cout << "Helloo from México" << endl;
+    cout << "Enter your favorite number: ";
+    cin >> num;
+    cout << "Your favorite number is: " << num << endl;
+    return 0;
+}
