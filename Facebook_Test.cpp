550cb724e00735f0ae7807239e0295f7ba10a6d2
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2021-09-30 12:10:13 -0500

    Add files via upload

---
+++ Facebook_Test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+int add(){
+    int param1=0,param2=0;
+    int paramx = 0;
+    cin>>param1; 
+    cin>>param2;
+    paramx = param1 + param2;
+    cout<<paramx<<endl;
+    return 0;
+}
