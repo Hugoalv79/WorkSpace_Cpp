1c1de3e5b93a509e0e32033bb5586ccd994b3b88
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2021-11-01 11:20:56 -0600

    Add files via upload

---
+++ CompilerWarningsMain.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// This is my first comment
+int main() {
+    
+    int favorite_number; // This is where  my favorite number is stored
+    
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
+
+/*  
+
+This is a multiple line comment
+xdxdxd
+:)
+*/
