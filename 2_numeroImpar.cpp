b8bbd06fbb1b19dee8eb34078961ece07a1da6a5
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2021-11-02 19:20:59 -0600

    Add files via upload

---
+++ 2_numeroImpar.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() 
+{   int user_impar;
+    do{printf("Ingresa un numero impar: "); scanf("%d",&user_impar);} 
+    while(user_impar%2==0);
+
+    printf("Gracias por teclear un valor impar , te haz ganado unos chilaquiles!");
+    
+   return 0; 
+}
+// Hugo Tonatiuh Alvarado Garcia - A00834109
