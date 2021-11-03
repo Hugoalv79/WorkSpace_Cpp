6aaae5981357832699d113846165b55cb5371e2c
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2021-11-03 09:37:18 -0600

    Add files via upload

---
+++ 3_sumaPares100.cpp
@@ -0,0 +1,17 @@
+
+//     sumaPares. 
+// El programa debe sumar y desplegar la suma de  todos los números enteros pares 
+// entre el 0 y el 100. 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{   int x = 0;
+    for(int i=2;i<=100;i+=2)
+        {x+=i;}
+    
+    cout<<x;
+    return 0;
+}
+
+// Hugo Tonatiuh Alvarado Garcia - A00834109
